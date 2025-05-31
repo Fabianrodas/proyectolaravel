87689e2f2cfc8aae771c993cef87f4417a4a7491
Fabián Rodas <fabianrodas2005@gmail.com>
2025-05-31 18:29:29 -0500

    Adding models, migrations and empty controllers

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,56 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+        'bio',
+        'image',
+        'is_private',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes() {
+        return $this->hasMany(Like::class);
+    }
+
+    public function followers() {
+        return $this->hasMany(Follow::class, 'followed_id');
+    }
+
+    public function following() {
+        return $this->hasMany(Follow::class, 'follower_id');
+    }
+
+    public function conversations1() {
+        return $this->hasMany(Conversation::class, 'user1_id');
+    }
+
+    public function conversations2() {
+        return $this->hasMany(Conversation::class, 'user2_id');
+    }
+
+    public function messages() {
+        return $this->hasMany(Message::class, 'sender_id');
+    }
 }
