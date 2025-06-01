4fd89ae020b331753987116878316c057cdaab16
Paula <paulamylenne2005@gmail.com>
2025-05-31 19:12:35 -0500

    Changing some migration values

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,8 +11,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
+            $table->string('username');
+            $table->string('email');
             $table->string('password');
             $table->text('bio')->nullable();    
             $table->string('image')->nullable(); 
